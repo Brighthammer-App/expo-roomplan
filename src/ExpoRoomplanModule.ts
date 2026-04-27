05bd688b971f49d3c84581727dc10ddc461e86e9
jalnyn <alan.yuan.jly@gmail.com>
2026-04-26 23:50:53 -0400

    update android stub

--- src/ExpoRoomplanModule.ts
+++ src/ExpoRoomplanModule.ts
@@ -6,6 +6,7 @@ const ExpoRoomplan: ExpoRoomPlanModuleType =
   Platform.OS === "ios"
     ? requireNativeModule<ExpoRoomPlanModuleType>("ExpoRoomPlan")
     : {
+        isSupported: false,
         startCapture: async () => { throw new Error("RoomPlan is not available on Android."); },
         stopCapture: async () => { throw new Error("RoomPlan is not available on Android."); },
       };
